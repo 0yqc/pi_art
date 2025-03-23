ac59f81580efd7ec2b1b3fcc175979aa7484d26b
0yqc <0yqc@duck.com>
2025-03-23 18:53:46 +0100

    Inital Commit, published this script.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pi_art
 A python tool to generate Charcter Art based on the digits of pi
+
+# Usage
+## Input
+These files in the same folder as the script:
+- image.png (Image to convert)
+- pi.txt 
---
+++ pi_art.py
@@ -0,0 +1,47 @@
+from PIL import Image # Read image contents
+
+pi_file = open("./pi.txt", "r", encoding="utf-8-sig") # Open file with decimal points of pi, You need enough decimal points to fill every non-transparent pixel in the image
+img_file = Image.open("./image.png") # Open the image file to be converted
+
+# One pixel gets converted to one digit, you need to scale down your image previously
+# Since one digit is not square, the image should be streched previously
+# Every pixel that is not fully transparent (alpha = 0) will be converted to a digit of pi
+
+pi = tuple(pi_file.read()) # Create a tuple with one digit per element
+img_width, img_height = img_file.size # Get the size of the image in width and height
+img_data = img_file.load() # Load the image data
+img = [] # Initialize List
+for y in range(img_height): # Loop through every row
+	for x in range(img_width): # Loop through every column
+		if len(img_data[x, y]) == 4: # RGBA value
+			n, n, n, a = img_data[x, y] # Ignoring everything except Alpha
+		elif len(img_data[x, y]) == 2: # LA value
+			n, a = img_data[x, y]
+		else: # Otherwise interpreting as not transparent
+			a = 255
+		
+		if a == 0: # Transparent Pixel > No Digit
+			img.append(False)
+		else: # Otherwise > Digit
+			img.append(True)
+	img.append("\n") # Go to a new line
+
+output = open("./output.txt", "w") # Clear output file (Overwrite evryything)
+output.close()
+output = open("./output.txt", "a") # Open for appending each new character
+n = 0 # Digit of Pi counter
+for i in range(len(img)): # Loop through img list
+	if img[i] == True: # True > Digit
+		print(pi[n], end="") # Print next digit of pi, end="" to prevent new line
+		output.write(pi[n]) # Write next digit of pi to output file
+		n += 1 # Increade pi digit counter for next digit
+	elif img[i] == False: # False > No Digit
+		print(" ", end="") # Print space, end="" to prevent new line
+		output.write(" ") # Write space to output file
+	elif img[i] == "\n": # If new line
+		print("") # Print nothing to enter new line
+		output.write("\n") # Write new line to output file
+
+pi_file.close()
+img_file.close()
+output.close()
